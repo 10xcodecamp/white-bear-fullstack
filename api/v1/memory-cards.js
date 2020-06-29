54e6c8686e5302c877cce80590eb83a3db1ada56
149203 <149203@gmail.com>
2020-06-29 12:09:37 -0700

    selectAllCards query

---
+++ api/v1/memory-cards.js
@@ -0,0 +1,28 @@
+// The memory-cards resource
+const express = require("express");
+const router = express.Router();
+const db = require("../../db");
+const selectAllCards = require("../../queries/selectAllCards");
+
+// @route      GET api/v1/memory-cards
+// @desc       Get all memory cards for a user by search term and order
+// @access     Public
+router.get("/", (req, res) => {
+   db.query(
+      selectAllCards(
+         "6781066b-a2a5-4670-b473-213eb446b101",
+         "ash",
+         "`memory_cards`.`created_at` ASC"
+      )
+   )
+      .then((dbRes) => {
+         console.log(dbRes);
+         res.json(dbRes);
+      })
+      .catch((err) => {
+         console.log(err);
+         res.status(400).json(err);
+      });
+});
+
+module.exports = router;
