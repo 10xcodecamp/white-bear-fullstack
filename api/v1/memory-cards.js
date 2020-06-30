84a1ea2642a0795456bcc07bd63bb2ee85016149
149203 <149203@gmail.com>
2020-06-30 13:58:48 -0700

    call API on AllCards; use toSqlString

--- api/v1/memory-cards.js
+++ api/v1/memory-cards.js
@@ -21,7 +21,7 @@ router.get("/", (req, res) => {
       userId,
       constructedSearchTerm,
       constructedSearchTerm,
-      order,
+      { toSqlString: () => order },
    ])
       .then((memoryCards) => {
          const mappedMemoryCards = memoryCards.map((memoryCard) => {
@@ -37,6 +37,7 @@ router.get("/", (req, res) => {
                level: memoryCard.level,
             };
          });
+         console.log(mappedMemoryCards);
          res.json(mappedMemoryCards);
       })
       .catch((err) => {
