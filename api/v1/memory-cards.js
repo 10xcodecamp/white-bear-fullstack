d34090e3e75e60d6b87e35743be09d6a6c3baea9
149203 <149203@gmail.com>
2020-06-30 11:08:58 -0700

    add prepared statement syntax

--- api/v1/memory-cards.js
+++ api/v1/memory-cards.js
@@ -10,7 +10,19 @@ const selectAllCards = require("../../queries/selectAllCards");
 router.get("/", (req, res) => {
    console.log(req.query);
    const { userId, searchTerm, order } = req.query;
-   db.query(selectAllCards(userId, searchTerm, order))
+   let constructedSearchTerm;
+   if (searchTerm === "" || searchTerm === undefined) {
+      constructedSearchTerm = "%%";
+   } else {
+      constructedSearchTerm = `%${searchTerm}%`;
+   }
+   /* https://www.npmjs.com/package/mysql#escaping-query-values */
+   db.query(selectAllCards, [
+      userId,
+      constructedSearchTerm,
+      constructedSearchTerm,
+      order,
+   ])
       .then((dbRes) => {
          res.json(dbRes);
       })
