a5747e321d864ffdab26b433e4fafc89785c1b56
149203 <149203@gmail.com>
2020-07-04 15:20:56 -0700

    add server-side validation to SignUp

--- api/v1/users.js
+++ api/v1/users.js
@@ -2,46 +2,57 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../db");
-const selectUser = require("../../queries/selectUser");
 const insertUser = require("../../queries/insertUser");
-const { toJson, toSafeParse, toHash } = require("../../utils/helpers");
-
-// @route      GET api/v1/users
-// @desc       Get a valid user via email and password
-// @access     Public
-router.get("/", (req, res) => {
-   db.query(selectUser("mike@gmail.com", "replace_me"))
-      .then((dbRes) => {
-         const user = toSafeParse(toJson(dbRes))[0];
-         console.log(user);
-         res.json(user);
-      })
-      .catch((err) => {
-         console.log(err);
-         res.status(400).json(err);
-      });
-});
+const selectUserById = require("../../queries/selectUserById");
+const { toHash } = require("../../utils/helpers");
+const getCreateEmailError = require("../../validation/getCreateEmailError");
+const getCreatePasswordError = require("../../validation/getCreatePasswordError");
 
 // @route      POST api/v1/users
 // @desc       Create a new user
 // @access     Public
 router.post("/", async (req, res) => {
-   const user = {
-      id: req.body.id,
-      email: req.body.email,
-      password: await toHash(req.body.password),
-      created_at: req.body.createdAt,
-   };
-
-   db.query(insertUser, user)
-      .then((dbRes) => {
-         console.log(dbRes);
-         // return the user data so we can put in redux store
-      })
-      .catch((err) => {
-         console.log(err);
-         // return a 400 error to user
-      });
+   const { id, email, password, createdAt } = req.body;
+   const emailError = await getCreateEmailError(email);
+   const passwordError = getCreatePasswordError(password, email);
+   let mysqlError = "";
+   if (emailError === "" && passwordError === "") {
+      const user = {
+         id,
+         email,
+         password: await toHash(password),
+         created_at: createdAt,
+      };
+      db.query(insertUser, user)
+         .then(() => {
+            // return the user data so we can put in redux store
+            db.query(selectUserById, id)
+               .then((users) => {
+                  const user = users[0];
+                  res.json({
+                     id: user.id,
+                     email: user.email,
+                     createdAt: user.created_at,
+                  });
+               })
+               .catch((err) => {
+                  mysqlError = `${err.code} ${err.sqlMessage}`;
+                  console.log(mysqlError);
+                  res.status(400).json({
+                     mysqlError,
+                     emailError,
+                     passwordError,
+                  });
+               });
+         })
+         .catch((err) => {
+            mysqlError = `${err.code} ${err.sqlMessage}`;
+            console.log(mysqlError);
+            res.status(400).json({ mysqlError, emailError, passwordError });
+         });
+   } else {
+      res.status(400).json({ mysqlError, emailError, passwordError });
+   }
 });
 
 module.exports = router;
--- client/src/components/ui/SignUp.js
+++ client/src/components/ui/SignUp.js
@@ -1,7 +1,6 @@
 import React from "react";
 import classnames from "classnames";
 import { v4 as getUuid } from "uuid";
-import { EMAIL_REGEX } from "../../utils/helpers";
 import { withRouter } from "react-router-dom";
 import axios from "axios";
 import actions from "../../store/actions";
@@ -25,94 +24,45 @@ class SignUp extends React.Component {
       });
    }
 
-   async setEmailState(emailInput) {
-      const lowerCasedEmailInput = emailInput.toLowerCase();
-      if (emailInput === "")
-         this.setState({
-            emailError: "Please enter your email address.",
-            hasEmailError: true,
-         });
-      else if (EMAIL_REGEX.test(lowerCasedEmailInput) === false) {
-         this.setState({
-            emailError: "Please enter a valid email address.",
-            hasEmailError: true,
-         });
-      } else {
-         this.setState({ emailError: "", hasEmailError: false });
-      }
-   }
-
-   checkHasLocalPart(passwordInput, emailInput) {
-      const localPart = emailInput.split("@")[0];
-      console.log(localPart);
-      if (localPart === "") return false;
-      else if (localPart.length < 4) return false;
-      else return passwordInput.includes(localPart);
-   }
-
-   async setPasswordState(passwordInput, emailInput) {
-      console.log(passwordInput);
-
-      const uniqChars = [...new Set(passwordInput)];
-      if (passwordInput === "") {
-         this.setState({
-            passwordError: "Please create a password.",
-            hasPasswordError: true,
-         });
-      } else if (passwordInput.length < 9) {
-         this.setState({
-            passwordError: "Your password must be at least 9 characters.",
-            hasPasswordError: true,
-         });
-      } else if (this.checkHasLocalPart(passwordInput, emailInput)) {
-         this.setState({
-            passwordError:
-               "For your safety, your password cannot contain your email address.",
-            hasPasswordError: true,
-         });
-      } else if (uniqChars.length < 3) {
-         this.setState({
-            passwordError:
-               "For your safety, your password must contain at least 3 unique characters.",
-            hasPasswordError: true,
-         });
-      } else {
-         this.setState({ passwordError: "", hasPasswordError: false });
-      }
-   }
-
    async validateAndCreateUser() {
       const emailInput = document.getElementById("signup-email-input").value;
       const passwordInput = document.getElementById("signup-password-input")
          .value;
-      console.log({ emailInput, passwordInput });
-      await this.setEmailState(emailInput);
-      await this.setPasswordState(passwordInput, emailInput);
-      if (
-         this.state.hasEmailError === false &&
-         this.state.hasPasswordError === false
-      ) {
-         // Create user obj
-         const user = {
-            id: getUuid(),
-            email: emailInput,
-            password: passwordInput,
-            createdAt: Date.now(),
-         };
-         console.log("Created user object for POST: ", user);
-         // post to API
-         axios
-            .post("/api/v1/users", user)
-            .then((res) => {
-               console.log(res);
-            })
-            .catch((err) => {
-               console.log(err);
-            });
 
-         // Update currentUser in global state with API response
-         // Go to next page: this.props.history.push("/create-answer");
-      }
+      // Create user obj
+      const user = {
+         id: getUuid(),
+         email: emailInput,
+         password: passwordInput,
+         createdAt: Date.now(),
+      };
+      console.log("Created user object for POST: ", user);
+      // post to API
+      axios
+         .post("/api/v1/users", user)
+         .then((res) => {
+            console.log(res.data);
+            this.props.dispatch({
+               type: actions.UPDATE_CURRENT_USER,
+               payload: res.data,
+            });
+            this.props.history.push("/create-answer");
+         })
+         .catch((err) => {
+            const data = err.response.data;
+            const { emailError, passwordError } = data;
+            console.log(data);
+            if (data.emailError !== "") {
+               this.setState({ hasEmailError: true, emailError });
+            } else {
+               this.setState({ hasEmailError: false, emailError });
+            }
+            if (data.passwordError !== "") {
+               this.setState({ hasPasswordError: true, passwordError });
+            } else {
+               this.setState({ hasPasswordError: false, passwordError });
+            }
+         });
    }
 
    render() {
