15039b4a14a4f5baea8452902d3542858f45f306
149203 <149203@gmail.com>
2020-07-17 11:04:17 -0700

    use custom file upload input

--- client/src/components/pages/S3.js
+++ client/src/components/pages/S3.js
@@ -2,29 +2,47 @@ import React from "react";
 import AppTemplate from "../ui/AppTemplate";
 
 class S3 extends React.Component {
+   constructor() {
+      super();
+      this.state = {
+         photoUploadText: "Choose a file",
+      };
+   }
+
+   setPhotoUploadText(e) {
+      const text = e.target.value;
+      this.setState({ photoUploadText: text });
+   }
+
    render() {
       return (
          <AppTemplate>
-            <h1>S3 Demo - User Profile</h1>
-            <label for="username">Create a username</label>
+            <h3 className="mt-6">S3 Demo</h3>
+            <h2>Update user profile</h2>
+            <label htmlFor="username">Create a username</label>
             <input
-               className="form-control form-control-sm mb-4"
+               className="form-control form-control-sm mb-6"
                type="text"
                id="username"
             />
-            {/* <label for="username">Upload your profile photo</label>
-            <input
-               className="form-control-file"
-               type="file"
-               id="photo-upload"
-            /> */}
 
-            <div class="custom-file">
-               <input type="file" class="custom-file-input" id="customFile" />
-               <label class="custom-file-label" for="customFile">
-                  Choose file
+            <div className="custom-file mb-6">
+               <input
+                  type="file"
+                  className="custom-file-input"
+                  id="photo-upload"
+                  onChange={(e) => {
+                     this.setPhotoUploadText(e);
+                  }}
+               />
+               <label className="custom-file-label" htmlFor="photo-upload">
+                  {this.state.photoUploadText}
                </label>
             </div>
+
+            <button className="btn btn-success float-right">
+               Save your profile
+            </button>
          </AppTemplate>
       );
    }
