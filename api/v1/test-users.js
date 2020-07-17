338fb3f410ea827b2d9a4870dcb364315080b883
149203 <149203@gmail.com>
2020-07-17 11:22:33 -0700

    fix router.post bug

--- api/v1/test-users.js
+++ api/v1/test-users.js
@@ -7,7 +7,7 @@ const validateJwt = require("../../utils/validateJwt");
 // @route      POST api/v1/test-users
 // @desc       Create a new test user in the test users resource
 // @access     Private
-router.get("/", validateJwt, (req, res) => {
+router.post("/", validateJwt, (req, res) => {
    const userId = req.user.id;
    console.log("made it to test-users route");
 });
